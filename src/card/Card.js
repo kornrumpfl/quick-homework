4df059caf36287943f5f67963af8cbbb7f009f7a
Luiz Kornrumpf <luiz.kornrumpf@gmail.com>
2022-09-27 20:33:23 +0200

    first positive result

--- src/card/Card.js
+++ src/card/Card.js
@@ -1,16 +1,21 @@
+import "./Card.css";
+
 function Person({name,src,status}){
-if (status==="Alive"){
+
+/* if (status==="Alive"){
     let color = "green";
 }else if (status==="dead"){
     let color = "red";
 }else{
     let color="grey";
-}
-    return(
-    <div className="Card" style={{ color: `${color}`}}>
+} */
+
+return(    
+    <div className="Card" style={{ background: `${status==="Alive"?"green":status==="Dead"?"red":"grey"}`}}>
         <img src={src} alt="fail to load"></img>
         <h2>{name}</h2>
     </div>
-)}
+);}
+
 
 export default Person;
