cac984310e5e7efc2c55107f717dd60b2d30fd8c
Luiz Kornrumpf <luiz.kornrumpf@gmail.com>
2022-09-30 01:01:22 +0200

    stuck

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 import './App.css';
-import { persons } from "./assets/persons";
 import Person from "./card/Card";
+import Select from 'react-select';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [dataFetched, setDataFetched] = useState([]);
+  const [currywurst, setCurrywurst] = useState([]);
+
+
+  console.log(currywurst);
+
+  const statusOptions = [
+    { value: 'unknown', label: 'unknown' },
+    { value: 'Alive', label: 'Alive' },
+    { value: 'Dead', label: 'Dead' }
+  ]
+
+  useEffect(()=>{
+    loadFetchedData()
+  },[])
+
+  async function loadFetchedData() {
+    try {
+      const response = await fetch("https://rickandmortyapi.com/api/character");
+      const data = await response.json();
+      setDataFetched(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
   return (
     <div className="App">
-          {persons.map((person_data)=>{
+      <Select 
+            placeholder= "Select a Status"
+            options={statusOptions}
+            setCurrywurst={entry=>{entry.map(o=>{return o.value})}}></Select>
+
+          {dataFetched.filter(brokkoly=>{return brokkoly.status===currywurst}).map((person_data)=>{
         return <Person key={person_data.id} name={person_data.name} src={person_data.image}
         status={person_data.status}/>
       })}
+
     </div>
   );
 }
--- src/card/Card.js
+++ src/card/Card.js
@@ -2,14 +2,6 @@ import "./Card.css";
 
 function Person({name,src,status}){
 
-/* if (status==="Alive"){
-    let color = "green";
-}else if (status==="dead"){
-    let color = "red";
-}else{
-    let color="grey";
-} */
-
 return(    
     <div className="Card" style={{ background: `${status==="Alive"?"green":status==="Dead"?"red":"grey"}`}}>
         <img src={src} alt="fail to load"></img>
